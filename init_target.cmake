60f9cc611aad098ab0d3769ae1bb84061ac4663f
John Preston <johnprestonmail@gmail.com>
2019-11-29 17:52:44 +0300

    Enable LTO by Xcode project settings.

--- init_target.cmake
+++ init_target.cmake
@@ -27,13 +27,18 @@ function(init_target target_name) # init_target(my_target folder_name)
     endif()
     target_link_libraries(${target_name} PUBLIC desktop-app::common_options)
     set_target_properties(${target_name} PROPERTIES
-        INTERPROCEDURAL_OPTIMIZATION_RELEASE True
-        INTERPROCEDURAL_OPTIMIZATION_RELWITHDEBINFO True
-        INTERPROCEDURAL_OPTIMIZATION_MINSIZEREL True
         LINK_SEARCH_START_STATIC 1
         XCODE_ATTRIBUTE_CLANG_ENABLE_OBJC_WEAK YES
         XCODE_ATTRIBUTE_GCC_INLINES_ARE_PRIVATE_EXTERN YES
         XCODE_ATTRIBUTE_GCC_SYMBOLS_PRIVATE_EXTERN YES
+        XCODE_ATTRIBUTE_GCC_OPTIMIZATION_LEVEL $<IF:$<CONFIG:Debug>,0,fast>
+        XCODE_ATTRIBUTE_LLVM_LTO $<IF:$<CONFIG:Debug>,NO,YES>
     )
+    if (NOT APPLE)
+        set_target_properties(${target_name} PROPERTIES
+            INTERPROCEDURAL_OPTIMIZATION_RELEASE True
+            INTERPROCEDURAL_OPTIMIZATION_RELWITHDEBINFO True
+            INTERPROCEDURAL_OPTIMIZATION_MINSIZEREL True
+        )
+    endif()
 endfunction()
-
