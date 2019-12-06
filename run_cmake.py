632ed315b2054c65954b3d4818b4b5326a0933e8
John Preston <johnprestonmail@gmail.com>
2019-12-06 13:12:04 +0300

    Introduce DESKTOP_APP_USE_PACKAGED.

---
+++ run_cmake.py
@@ -0,0 +1,54 @@
+'''
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+'''
+import sys, os, shutil, subprocess
+
+def run(project, arguments):
+    scriptPath = os.path.dirname(os.path.realpath(__file__))
+    basePath = scriptPath + '/../out'
+    
+    cmake = ['cmake']
+    for arg in arguments:
+        if arg == 'debug':
+            cmake.append('-DCMAKE_BUILD_TYPE=Debug')
+        elif arg != 'fresh':
+            cmake.append(arg)
+    
+    if sys.platform == 'win32':
+        cmake.append('-AWin32')
+    elif sys.platform != 'darwin':
+        if not '-DCMAKE_BUILD_TYPE=Debug' in cmake:
+            cmake.append('-DCMAKE_BUILD_TYPE=Release')
+
+    specialTarget = ''
+    specialTargetFile = scriptPath + '/../' + project + '/build/target'
+    if os.path.isfile(specialTargetFile):
+        with open(specialTargetFile, 'r') as f:
+            for line in f:
+                target = line.strip()
+                if len(target) > 0:
+                    cmake.append('-DDESKTOP_APP_SPECIAL_TARGET=' + target)
+
+    cmake.extend(['-Werror=dev', '-Werror=deprecated', '--warn-uninitialized', '..'])
+    command = ' '.join(cmake)
+
+    if not os.path.exists(basePath):
+        os.mkdir(basePath)
+    elif 'fresh' in arguments:
+        paths = os.listdir(basePath)
+        for path in paths:
+            if path.lower().startswith('cmake'):
+                full = basePath + '/' + path
+                if os.path.isdir(full):
+                    shutil.rmtree(full, ignore_errors=False)
+                else:
+                    os.remove(full)
+        print('Cleared previous.')
+    os.chdir(basePath)
+    subprocess.call(command, shell=True)
+
+    return 0
