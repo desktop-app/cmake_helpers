b087501d66ea13395acd91397b3ab6a3a77c41b8
John Preston <johnprestonmail@gmail.com>
2020-01-02 14:20:14 +0300

    Fix whitespace and indentation errors.

--- linux_glibc_wraps/platform/linux/linux_glibc_wraps_64.c
+++ linux_glibc_wraps/platform/linux/linux_glibc_wraps_64.c
@@ -22,31 +22,31 @@ __asm__(".symver __clock_gettime_glibc_old,clock_gettime@GLIBC_" GETTIME_GLIBC_V
 
 
 int __wrap_clock_gettime(clockid_t clk_id, struct timespec *tp) {
-        return __clock_gettime_glibc_old(clk_id, tp);
+	return __clock_gettime_glibc_old(clk_id, tp);
 }
 
 UTItype __udivmodti4(UTItype num, UTItype den, UTItype *rem_p);
 
 TItype __wrap___divmodti4(TItype num, TItype den, TItype *rem_p) {
-        int minus = 0;
-        TItype v;
+	int minus = 0;
+	TItype v;
 
-        if (num < 0) {
-                num = -num;
-                minus = 1;
-        }
-        if (den < 0) {
-                den = -den;
-                minus ^= 1;
-        }
+	if (num < 0) {
+		num = -num;
+		minus = 1;
+	}
+	if (den < 0) {
+		den = -den;
+		minus ^= 1;
+	}
 
-        v = __udivmodti4(num, den, (UTItype *)rem_p);
-        if (minus) {
-                v = -v;
-                if (rem_p)
-                        *rem_p = -(*rem_p);
-        }
+	v = __udivmodti4(num, den, (UTItype *)rem_p);
+	if (minus) {
+		v = -v;
+		if (rem_p)
+			*rem_p = -(*rem_p);
+	}
 
-        return v;
+	return v;
 }
 
