2fb8a0c2af720bad7e105b80c8e72d5fcacc63df
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-05-08 15:31:50 +0400

    Add fcitx5-qt and it's dependencies

--- external/qt/package.cmake
+++ external/qt/package.cmake
@@ -25,9 +25,9 @@ find_package(Qt5Gui COMPONENTS QWebpPlugin REQUIRED)
 
 if (LINUX)
     if (NOT DESKTOP_APP_USE_PACKAGED)
-        find_package(Qt5 COMPONENTS WaylandClient Svg REQUIRED)
+        find_package(Qt5 COMPONENTS WaylandClient Svg X11Extras REQUIRED)
     elseif (DESKTOP_APP_USE_PACKAGED_LAZY)
-        find_package(Qt5 COMPONENTS WaylandClient REQUIRED)
+        find_package(Qt5 COMPONENTS WaylandClient X11Extras REQUIRED)
 
         if (DESKTOP_APP_USE_PACKAGED_LAZY_PLATFORMTHEMES)
             find_package(Qt5 COMPONENTS Svg REQUIRED)
