995b1e4cbd4ba0c02cb7432401bef029f480d69b
John Preston <johnprestonmail@gmail.com>
2021-08-24 20:11:49 +0300

    Remove /fp:except in Windows 32 bit build.

--- options_win.cmake
+++ options_win.cmake
@@ -45,10 +45,10 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
             /bigobj # scheme.cpp has too many sections.
         )
     else()
-        target_compile_options(common_options
-        INTERFACE
-            /fp:except # Crash-report fp exceptions in 32 bit build.
-        )
+        # target_compile_options(common_options
+        # INTERFACE
+        #     /fp:except # Crash-report fp exceptions in 32 bit build.
+        # )
         target_link_options(common_options
         INTERFACE
             /LARGEADDRESSAWARE # Allow more than 2 GB in 32 bit application.
