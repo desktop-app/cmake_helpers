6ba60e2a865cbb2abc3f38c5ee81580c2c8b36f4
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-08-08 19:14:33 +0400

    Remove unused now wayland symbols

--- linux_wayland_helper/linux_wayland_helper.cpp
+++ linux_wayland_helper/linux_wayland_helper.cpp
@@ -53,9 +53,6 @@ int (*wl_cursor_frame_and_duration)(
 
 void (*wl_proxy_destroy)(struct wl_proxy *proxy);
 uint32_t (*wl_proxy_get_version)(struct wl_proxy *proxy);
-void (*wl_array_init)(struct wl_array *array);
-void (*wl_array_release)(struct wl_array *array);
-void *(*wl_array_add)(struct wl_array *array, size_t size);
 int (*wl_proxy_add_listener)(
 	struct wl_proxy *proxy,
 	void (**implementation)(void),
@@ -75,16 +72,13 @@ int (*wl_display_prepare_read_queue)(
 int (*wl_display_flush)(struct wl_display *display);
 void (*wl_display_cancel_read)(struct wl_display *display);
 int (*wl_display_read_events)(struct wl_display *display);
-void (*wl_event_queue_destroy)(struct wl_event_queue *queue);
 int (*wl_display_prepare_read)(struct wl_display *display);
 int (*wl_display_dispatch_pending)(struct wl_display *display);
 struct wl_display *(*wl_display_connect)(const char *name);
-struct wl_display *(*wl_display_connect_to_fd)(int fd);
 void (*wl_display_disconnect)(struct wl_display *display);
 void *(*wl_proxy_create_wrapper)(void *proxy);
 void (*wl_proxy_wrapper_destroy)(void *proxy_wrapper);
 void (*wl_proxy_set_queue)(struct wl_proxy *proxy, struct wl_event_queue *queue);
-int (*wl_display_roundtrip)(struct wl_display *display);
 struct wl_proxy *(*wl_proxy_marshal_array_constructor)(
 	struct wl_proxy *proxy,
 	uint32_t opcode,
@@ -230,9 +224,6 @@ bool Resolve() {
 			&& LoadLibrary(client, "libwayland-client.so.0")
 			&& LOAD_SYMBOL(client, wl_proxy_destroy)
 			&& LOAD_SYMBOL(client, wl_proxy_get_version)
-			&& LOAD_SYMBOL(client, wl_array_init)
-			&& LOAD_SYMBOL(client, wl_array_release)
-			&& LOAD_SYMBOL(client, wl_array_add)
 			&& LOAD_SYMBOL(client, wl_proxy_add_listener)
 			&& LOAD_SYMBOL(client, wl_proxy_set_user_data)
 			&& LOAD_SYMBOL(client, wl_proxy_get_user_data)
@@ -245,16 +236,13 @@ bool Resolve() {
 			&& LOAD_SYMBOL(client, wl_display_flush)
 			&& LOAD_SYMBOL(client, wl_display_cancel_read)
 			&& LOAD_SYMBOL(client, wl_display_read_events)
-			&& LOAD_SYMBOL(client, wl_event_queue_destroy)
 			&& LOAD_SYMBOL(client, wl_display_prepare_read)
 			&& LOAD_SYMBOL(client, wl_display_dispatch_pending)
 			&& LOAD_SYMBOL(client, wl_display_connect)
-			&& LOAD_SYMBOL(client, wl_display_connect_to_fd)
 			&& LOAD_SYMBOL(client, wl_display_disconnect)
 			&& LOAD_SYMBOL(client, wl_proxy_create_wrapper)
 			&& LOAD_SYMBOL(client, wl_proxy_wrapper_destroy)
 			&& LOAD_SYMBOL(client, wl_proxy_set_queue)
-			&& LOAD_SYMBOL(client, wl_display_roundtrip)
 			&& LOAD_SYMBOL(client, wl_proxy_marshal_array_constructor)
 			&& LOAD_SYMBOL(client, wl_proxy_marshal_array_constructor_versioned)
 			&& LOAD_SYMBOL(client, wl_proxy_get_id)
@@ -357,24 +345,6 @@ uint32_t wl_proxy_get_version(struct wl_proxy *proxy) {
 	return W::wl_proxy_get_version(proxy);
 }
 
-void wl_array_init(struct wl_array *array) {
-	Expects(W::wl_array_init != nullptr);
-
-	W::wl_array_init(array);
-}
-
-void wl_array_release(struct wl_array *array) {
-	Expects(W::wl_array_release != nullptr);
-
-	W::wl_array_release(array);
-}
-
-void *wl_array_add(struct wl_array *array, size_t size) {
-	Expects(W::wl_array_add != nullptr);
-
-	return W::wl_array_add(array, size);
-}
-
 int wl_proxy_add_listener(
 		struct wl_proxy *proxy,
 		void (**implementation)(void),
@@ -454,12 +424,6 @@ int wl_display_read_events(struct wl_display *display) {
 	return W::wl_display_read_events(display);
 }
 
-void wl_event_queue_destroy(struct wl_event_queue *queue) {
-	Expects(W::wl_event_queue_destroy != nullptr);
-
-	W::wl_event_queue_destroy(queue);
-}
-
 int wl_display_prepare_read(struct wl_display *display) {
 	Expects(W::wl_display_prepare_read != nullptr);
 
@@ -478,14 +442,6 @@ struct wl_display *wl_display_connect(const char *name) {
 		return nullptr;
 	}
 	return W::wl_display_connect(name);
-}
-
-struct wl_display *wl_display_connect_to_fd(int fd) {
-	if (!W::Resolve()) {
-		errno = ENOENT;
-		return nullptr;
-	}
-	return W::wl_display_connect_to_fd(fd);
 }
 
 void wl_display_disconnect(struct wl_display *display) {
@@ -512,12 +468,6 @@ void wl_proxy_set_queue(
 	Expects(W::wl_proxy_set_queue != nullptr);
 
 	W::wl_proxy_set_queue(proxy, queue);
-}
-
-int wl_display_roundtrip(struct wl_display *display) {
-	Expects(W::wl_display_roundtrip != nullptr);
-
-	return W::wl_display_roundtrip(display);
 }
 
 struct wl_proxy *wl_proxy_marshal_array_constructor(
