2ba61d5977407eceb8c9ff98b6a5de909a2c97ec
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-10-22 01:50:18 +0400

    Fix default kcoreaddons logging level

--- external/kcoreaddons/headers/private/kcoreaddons_debug.h
+++ external/kcoreaddons/headers/private/kcoreaddons_debug.h
@@ -1,3 +1,3 @@
 #pragma once
 #include <QLoggingCategory>
-static const QLoggingCategory KCOREADDONS_DEBUG("kf.coreaddons");
+static const QLoggingCategory KCOREADDONS_DEBUG("kf.coreaddons", QtInfoMsg);
