25088a573bb57c4a1d208a38cfc994e7206fb35b
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-11-24 08:31:52 +0400

    Revert "Add external_sonnet library"
    This reverts commit 81705fb6095ec3aa74ef900a1a181c0d586447d8.

--- external/sonnet/headers/private/hunspelldebug.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <QLoggingCategory>
-static const QLoggingCategory SONNET_HUNSPELL("kf.sonnet.clients.hunspell", QtInfoMsg);
--- external/sonnet/headers/private/nsspellcheckerdebug.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <QLoggingCategory>
-static const QLoggingCategory SONNET_NSSPELLCHECKER("kf.sonnet.clients.nsspellchecker", QtInfoMsg);
