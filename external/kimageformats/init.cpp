70f1fb6deadbfcffdbb047a86fcffbdf676ac698
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-06-17 02:50:51 +0400

    Migrate from qt_static_plugins.cpp to the Qt6-pregenerated initializators

---
+++ external/kimageformats/init.cpp
@@ -0,0 +1,12 @@
+/*
+This file is part of Desktop App Toolkit,
+a set of libraries for developing nice desktop applications.
+
+For license and copyright information please follow this link:
+https://github.com/desktop-app/legal/blob/master/LEGAL
+*/
+#include <QtCore/QtPlugin>
+
+Q_IMPORT_PLUGIN(QAVIFPlugin)
+Q_IMPORT_PLUGIN(HEIFPlugin)
+Q_IMPORT_PLUGIN(QJpegXLPlugin)
