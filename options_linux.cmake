4af3baf8e64771bfb5b96a923cf4e95fa372a4c1
art <abelromeroperez1990@gmail.com>
2024-07-18 15:54:33 +0200

    Added support to equivalent -fcf-protection but for aarch64

--- options_linux.cmake
+++ options_linux.cmake
@@ -85,6 +85,10 @@ if (NOT DESKTOP_APP_USE_PACKAGED OR DESKTOP_APP_SPECIAL_TARGET)
         -fstack-clash-protection
         -fcf-protection
     )
+    if (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+        list(REMOVE_ITEM common_options "-fcf-protection")
+        list(APPEND common_options "-msign-return-address=all" "-mbranch-protection=standard")
+    endif()
     target_link_options(common_options
     INTERFACE
         -Wl,-z,relro
