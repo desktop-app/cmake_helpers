61e405a8029f0e68f4632723264fe6f6c6a83c65
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-05-12 00:14:15 +0000

    Use NOT generator expresion on Windows

--- init_target.cmake
+++ init_target.cmake
@@ -52,7 +52,7 @@ function(init_target target_name) # init_target(my_target [cxx_std_..] folder_na
     endif()
     if (DESKTOP_APP_SPECIAL_TARGET)
         if (MSVC)
-            set_property(TARGET ${target_name} APPEND_STRING PROPERTY STATIC_LIBRARY_OPTIONS "$<IF:$<CONFIG:Debug>,,/LTCG>")
+            set_property(TARGET ${target_name} APPEND_STRING PROPERTY STATIC_LIBRARY_OPTIONS "$<$<NOT:$<CONFIG:Debug>>:/LTCG>")
         elseif (APPLE)
             set_target_properties(${target_name} PROPERTIES
                 XCODE_ATTRIBUTE_GCC_OPTIMIZATION_LEVEL $<IF:$<CONFIG:Debug>,0,fast>
