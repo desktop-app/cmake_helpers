c2eec4c04d9643e1e51f815ee157dca1dc2c7884
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-05-13 03:49:08 +0000

    Decouple lottie recoloring from DESKTOP_APP_USE_PACKAGED_RLOTTIE

--- variables.cmake
+++ variables.cmake
@@ -22,6 +22,7 @@ endif()
 
 set(CMAKE_CXX_SCAN_FOR_MODULES OFF CACHE BOOL "")
 option(DESKTOP_APP_TEST_APPS "Build test apps, development only." OFF)
+option(DESKTOP_APP_LOTTIE_DISABLE_RECOLORING "Disable recoloring of lottie animations." OFF)
 option(DESKTOP_APP_LOTTIE_USE_CACHE "Use caching in lottie animations." ON)
 cmake_dependent_option(DESKTOP_APP_DISABLE_X11_INTEGRATION "Disable all code for X11 integration." OFF LINUX ON)
 cmake_dependent_option(DESKTOP_APP_USE_ALLOCATION_TRACER "Use simple allocation tracer." OFF LINUX OFF)
