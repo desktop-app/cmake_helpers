62e82119aae8f1aae4ea1e0f3592146981443b5d
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-05-16 03:52:49 +0000

    Make DESKTOP_APP_MAC_ARCH inheritance more straightforward

--- init_target.cmake
+++ init_target.cmake
@@ -71,7 +71,7 @@ endfunction()
 # This code is not supposed to run on build machine, only on target machine.
 function(init_non_host_target target_name)
     init_target(${ARGV})
-    if (APPLE)
+    if (DESKTOP_APP_MAC_ARCH)
         set_target_properties(${target_name} PROPERTIES
             OSX_ARCHITECTURES "${DESKTOP_APP_MAC_ARCH}"
         )
