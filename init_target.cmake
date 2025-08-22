483a10954b0fafa7f759a82847b066c44466074c
John Preston <johnprestonmail@gmail.com>
2025-08-22 21:17:08 +0400

    Fix build with new Xcode.

--- init_target.cmake
+++ init_target.cmake
@@ -48,7 +48,6 @@ function(init_target target_name) # init_target(my_target [cxx_std_..] folder_na
             set_property(TARGET ${target_name} APPEND_STRING PROPERTY STATIC_LIBRARY_OPTIONS "$<$<NOT:$<CONFIG:Debug>>:/LTCG>")
         elseif (APPLE)
             set_target_properties(${target_name} PROPERTIES
-                XCODE_ATTRIBUTE_GCC_OPTIMIZATION_LEVEL $<IF:$<CONFIG:Debug>,0,fast>
                 XCODE_ATTRIBUTE_LLVM_LTO $<IF:$<CONFIG:Debug>,NO,YES>
             )
         else()
